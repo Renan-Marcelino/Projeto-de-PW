388c333a872cd2eb7b25fa50f3593f645508f151
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-11-10 23:43:36 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -50,13 +50,29 @@ function executarFuncao() {
   } else {
     funcaoOriginal();
   }
+  function confirmar() {
+    confirmacao = true;
+    localStorage.setItem('confirmacao', true);
+    console.log('Variável confirmacao definida como true');
 }
 
-function confirmar() {
-  confirmacao = true;
-  fecharPopup();
-}
+// Adicionar um ouvinte de evento DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecionar o formulário
+    var form = document.getElementById('myForm');
 
+    // Adicionar um ouvinte de evento submit ao formulário
+    form.addEventListener('submit', function (event) {
+        // Executar a função confirmar
+        confirmar();
+        
+        // Aqui você pode adicionar outras lógicas relacionadas ao envio do formulário
+
+        // Impedir o envio do formulário tradicional
+        event.preventDefault();
+    });
+});
+}
 
 // FIM da funçao para abrir tela LOGIN #RENAN
 
@@ -192,3 +208,70 @@ botaoTrocarImagem.addEventListener('click', function() {
         imagem.style.maxWidth = '100%'; // Volta para o tamanho original se for outra imagem
     }
 });
+
+
+
+    // Função para inicializar o formulário
+    function initializeForm() {
+        // Recuperar o nome armazenado no localStorage
+        var nomeArmazenado = localStorage.getItem('storedName');
+
+        // Exibir o nome na tela se existir
+        if (nomeArmazenado) {
+            document.getElementById('displayNome').innerText = "Nome armazenado: " + nomeArmazenado;
+        }
+    }
+
+    function validateForm() {
+        localStorage.setItem('confirmacao', true);
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        var confirmPassword = document.getElementById('confirmPassword').value;
+
+        // Validar nome sem caracteres especiais
+        var nameRegex = /^[a-zA-Z ]*$/;
+        if (!nameRegex.test(name)) {
+            displayError("Nome inválido. Use apenas letras e espaços.");
+            return false;
+        }
+
+        // Validar email
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            displayError("Email inválido. Insira um email válido.");
+            return false;
+        }
+
+        // Validar senha
+        if (password.length < 6) {
+            displayError("A senha deve ter pelo menos 6 caracteres.");
+            return false;
+        }
+
+        // Confirmar senha
+        if (password !== confirmPassword) {
+            displayError("As senhas não coincidem. Por favor, revise.");
+            return false;
+        }
+
+        // Armazenar nome no localStorage
+        localStorage.setItem('storedName', name);
+
+        // Exibir nome na tela após envio do formulário
+        document.getElementById('displayNome').innerText = "Nome armazenado: " + name;
+
+        // Limpar mensagens de erro, se houver
+        document.getElementById('error').innerHTML = "";
+
+        // Evitar o envio do formulário tradicional
+        return false;
+    }
+
+    function displayError(message) {
+        document.getElementById('error').innerHTML = "<p class='error'>" + message + "</p>";
+    }
+
+window.onload = initializeForm;
+
+
