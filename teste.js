f525d6b6d27cb3133a59af48804f6e192551acf8
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-11-17 17:13:46 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -38,7 +38,7 @@ function abrirContato() {
     var conteudoPopup = document.getElementById("conteudoPopup");
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "TelaEntrar.html", true);
+    xhr.open("GET", "TelaContato.html", true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             conteudoPopup.innerHTML = xhr.responseText;
